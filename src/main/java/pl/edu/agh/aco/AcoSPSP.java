39442bd8136e4c7a78528a5e33369c52a9038429
Nesbite <grzegorz.siatka@gmail.com>
2016-04-17 13:16:25 +0200

    Init parameters

--- src/main/java/pl/edu/agh/aco/AcoSPSP.java
+++ src/main/java/pl/edu/agh/aco/AcoSPSP.java
@@ -1,4 +1,22 @@
 package pl.edu.agh.aco;
 
 public class AcoSPSP {
+    private int employees;
+    private int tasks;
+    private int skills;
+    private int[] tasksDurationMatrix;
+    private int[][] skillsTasksMatrix;
+    private int[][] skillsEmployeesMatrix;
+    private int[][] taskDependencyMatrix;
+    private int[][] result;
+
+    private void init(){
+        this.tasksDurationMatrix = new int[tasks];
+        this.skillsEmployeesMatrix = new int[skills][employees];
+        this.taskDependencyMatrix = new int[tasks][tasks];
+        this.skillsTasksMatrix = new int[skills][tasks];
+        this.result = new int[tasks][employees];
+    }
+
+
 }
